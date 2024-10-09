4b81b61ffd87d279bd84f51fb79a012b95bd8ac5
Fabiana Tacco <95270412+fmstacco@users.noreply.github.com>
2024-10-09 15:12:07 +0000

    Create static and js folder and index.js file

---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+async function enviarMensagem() {
+    if(input.value == "" || input.value == null) return;
+    let mensagem = input.value;
+    input.value = "";
+
+    let novaBolha = criaBolhaUsuario();
+    novaBolha.innerHTML = mensagem;
+    chat.appendChild(novaBolha);
+
+    let novaBolhaBot = criaBolhaBot();
+    chat.appendChild(novaBolhaBot);
+    vaiParaFinalDoChat();
+    novaBolhaBot.innerHTML = "Analisando ..."
+    
+    // Envia requisição com a mensagem para a API do chatbot
+    const resposta = await fetch("http://127.0.0.1:5000/chat", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({'msg':mensagem}),
+    });
+    const textoDaResposta = await resposta.text();
+    console.log(textoDaResposta);
+    novaBolhaBot.innerHTML = textoDaResposta.replace(/\n/g, '<br>');
+    vaiParaFinalDoChat();
+}
